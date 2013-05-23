c01844b06f68b14d9f35fa8d2320e19f2c5177c6
tricton <tricton@gmail.com>
2013-05-23 15:18:21 +0300

    passed test for identity of components on main page

--- 42CCApp/CCMainPage.m
+++ 42CCApp/CCMainPage.m
@@ -7,6 +7,7 @@
 //
 
 #import "CCMainPage.h"
+#import "CCMe.h"
 
 @implementation CCMainPage
 
@@ -18,6 +19,21 @@
 }
 
 - (void)viewDidLoad{
+    NSString *name = [NSString stringWithFormat:@"%@ %@", [CCMe myData].name, [CCMe myData].surName ];
+    NSString *birth = [CCMe myData].birthDay;
+    NSString *bio = [CCMe myData].biography;
+    NSString *contacts = [NSString stringWithFormat:@"%@ \n %@ \n %@ \n if you want to see where i live - %@", [CCMe myData].address, [CCMe myData].phone, [CCMe myData].email, [CCMe myData].coordinates];
+    NSArray *labelText = @[name, bio, birth, contacts];
+    for (int label=0; label<[labelText count]; label++){
+        UILabel *infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 150+label*60, 300, 50)];
+        infoLabel.text = labelText[label];
+        infoLabel.tag = label+10;
+        [self.view addSubview:infoLabel];
+    }
+    UIImageView *myPhoto = [[UIImageView alloc] initWithImage:[CCMe myData].myPhoto];
+    myPhoto.frame = CGRectMake(106, 10, 128, 128);
+    myPhoto.tag = 20;
+    [self.view addSubview:myPhoto];
 }
 
 @end
