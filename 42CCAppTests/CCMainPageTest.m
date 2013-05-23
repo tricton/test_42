adc3a8cb9d545408dfc6cf09b35e5fa970c5e800
tricton <tricton@gmail.com>
2013-05-23 09:38:33 +0300

    Adding newly created project to git

---
+++ 42CCAppTests/CCMainPageTest.m
@@ -0,0 +1,13 @@
+//
+//  CCMainPageTest.m
+//  42CCApp
+//
+//  Created by Mykola Kamysh on 23.05.13.
+//  Copyright (c) 2013 Mykola Kamysh. All rights reserved.
+//
+
+#import "CCMainPageTest.h"
+
+@implementation CCMainPageTest
+
+@end
