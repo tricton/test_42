8b00eda7158115ace98290a5a56866550ed74434
tricton <tricton@gmail.com>
2013-05-24 10:22:18 +0300

    failing test to exactly conform view's frames after rotation to needed frames

--- 42CCAppTests/CCTabBarTest.m
+++ 42CCAppTests/CCTabBarTest.m
@@ -51,6 +51,58 @@ describe(@"Main page must contain my photo, bioraphy, contact data and name in 1
                 [[viewFramesPortrait[frame] shouldNot] equal:viewFramesLandscape[frame]];
             }
         });
+        it(@"Frames on main view must be equal to some certain values, sracified from some requirements", ^{
+            NSMutableArray *viewFramesPortrait = [NSMutableArray array];
+            NSMutableArray *viewFramesLandscape = [NSMutableArray array];
+            NSValue *portraitRect;
+            NSValue *landscapeRect;
+            NSValue *photoFrame;
+            NSValue *nameFrame;
+            NSValue *birthFrame;
+            NSValue *contactsFrame;
+            NSValue *bioFrame;
+            NSArray *expectedFramesPortrait = [NSArray array];
+            NSArray *expectedFramesLandscape = [NSArray array];
+            [mainPage changeViewFrames:UIInterfaceOrientationPortrait];
+            if (UIInterfaceOrientationMaskPortrait){
+                photoFrame = [NSValue valueWithCGRect:CGRectMake(96, 20, 128, 128)];
+                nameFrame = [NSValue valueWithCGRect:CGRectMake(50, 160, 220, 20)];
+                birthFrame = [NSValue valueWithCGRect:CGRectMake(50, 190, 220, 20)];
+                contactsFrame = [NSValue valueWithCGRect:CGRectMake(20, 210, 280, 80)];
+                bioFrame = [NSValue valueWithCGRect:CGRectMake(20, 300, 280, 100)];
+                expectedFramesPortrait = @[photoFrame, nameFrame, birthFrame, contactsFrame, bioFrame];
+                UIImageView *myPhotoPortrait = (UIImageView *)[mainPage.view viewWithTag:20];
+                portraitRect = [NSValue valueWithCGRect:myPhotoPortrait.frame];
+                for (int labelTag=0; labelTag<4; labelTag++){
+                    UILabel *label = (UILabel *)[mainPage.view viewWithTag:labelTag+10];
+                    [viewFramesPortrait addObject:[NSValue valueWithCGRect:label.frame]];
+                }
+                [viewFramesPortrait insertObject:landscapeRect
+                                          atIndex:0];
+            }
+            [mainPage changeViewFrames:UIInterfaceOrientationLandscapeRight];
+            if (UIInterfaceOrientationMaskLandscape){
+                photoFrame = [NSValue valueWithCGRect:CGRectMake(20, 20, 128, 128)];
+                nameFrame = [NSValue valueWithCGRect:CGRectMake(20, 160, 128, 20)];
+                birthFrame = [NSValue valueWithCGRect:CGRectMake(20, 190, 128, 20)];
+                contactsFrame = [NSValue valueWithCGRect:CGRectMake(170, 20, 270, 80)];
+                bioFrame = [NSValue valueWithCGRect:CGRectMake(170, 110, 270, 130)];
+                expectedFramesLandscape = @[photoFrame, nameFrame, birthFrame, contactsFrame, bioFrame];
+                UIImageView *myPhotoLandscape = (UIImageView *)[mainPage.view viewWithTag:20];
+                landscapeRect = [NSValue valueWithCGRect:myPhotoLandscape.frame];
+                for (int labelTag=0; labelTag<4; labelTag++){
+                    UILabel *label = (UILabel *)[mainPage.view viewWithTag:labelTag+10];
+                    [viewFramesLandscape addObject:[NSValue valueWithCGRect:label.frame]];
+                }
+                    [viewFramesLandscape insertObject:landscapeRect
+                                              atIndex:0];
+            }
+            for (int frame=0; frame<5; frame++){
+                [[viewFramesPortrait[frame] shouldNot] equal:expectedFramesPortrait[frame]];
+                [[viewFramesLandscape[frame] shouldNot] equal:expectedFramesLandscape[frame]];
+            }
+
+        });
     });
 });
 
