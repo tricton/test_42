58ad439cea53b1e04acf50497c1dec1c48657333
tricton <tricton@gmail.com>
2013-05-26 15:57:33 +0300

    check: if session is open, then root view controller must be tab bar, if session is closed - root view controller should not be the tab bar

---
+++ 42CCApp/FBLoginView+session.h
@@ -0,0 +1,13 @@
+//
+//  FBLoginView+session.h
+//  42CCApp
+//
+//  Created by Mykola Kamysh on 26.05.13.
+//  Copyright (c) 2013 Mykola Kamysh. All rights reserved.
+//
+
+#import "FBLoginView.h"
+
+@interface FBLoginView (session)
+
+@end
--- 42CCAppTests/CCFBTest.m
+++ 42CCAppTests/CCFBTest.m
@@ -4,28 +4,39 @@
 #import "CCAppDelegate.h"
 #import <FacebookSDK/FBSessionTokenCachingStrategy.h>
 #import <FacebookSDK/FBLoginView.h>
-#import "FBLoginView+LIttlePatch.h"
+#import "FBLoginView+session.h"
 
 #define appDelegate (CCAppDelegate *)[[UIApplication sharedApplication] delegate]
 
 SPEC_BEGIN(FB_checking)
 
 describe(@"After application start controller CCFBLogin must be active", ^{
-    __block CCFBLogin *currentController = (CCFBLogin *)[appDelegate window].rootViewController;
     context(@"CCFBLogin should not give the  user go further inside appllication", ^{
+        __block CCFBLogin *currentController = (CCFBLogin *)[appDelegate window].rootViewController;
         it(@"Further using of application user should get after login", ^{
             [[currentController should] beKindOfClass:[CCFBLogin class]];
         });
-        it(@"CCFBlogin should present button for authentificate in facebook. After pressing button sholud launch another app to log in to FB", ^{
+        it(@"CCFBlogin should present button for authentificate in facebook.", ^{
             UIView *loginButton = (UIButton *)[currentController.view viewWithTag:30];
             [[loginButton should] beKindOfClass:[FBLoginView class]];
-            [[theValue([appDelegate session].isOpen) should] equal:theValue(YES)];
+        });
+        it(@"Session to FB should be open to get token. App should not enter to further work with it without login to facebook", ^{
+            FBLoginView *loginView = (FBLoginView *)[currentController.view viewWithTag:30];
+            [[theValue([loginView session].isOpen) should] equal:theValue(YES)];
+            if ([loginView session]){
+                [[[appDelegate window].rootViewController should] equal:[appDelegate tabBarController]];
+            }
         });
         it(@"App must have a token to store it inside to further user login", ^{
             FBSessionTokenCachingStrategy *tokenCache = [[FBSessionTokenCachingStrategy alloc] initWithUserDefaultTokenInformationKeyName:nil];
             [[[[tokenCache fetchFBAccessTokenData] dictionary] objectForKey:@"com.facebook.sdk:TokenInformationTokenKey"] shouldNotBeNil];
         });
     });
+//    context(@"Testing app to login parameters", ^{
+//        it(@"App should not enter to further work with it without login to facebook", ^{
+//            [[[appDelegate window].rootViewController should] equal:[appDelegate tabBarController]];
+//        });
+//    });
 });
 
 SPEC_END
