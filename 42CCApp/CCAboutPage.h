2f00a6de382c5af0929bb7efed41321d2553a7d9
tricton <tricton@gmail.com>
2013-05-29 18:47:05 +0300

    ticket#3: have text about in userdefaults

--- 42CCApp/CCAboutPage.h
+++ 42CCApp/CCAboutPage.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCAboutPage : UIViewController
+@interface CCAboutPage : UIViewController<UITextViewDelegate>
 
 @end
--- 42CCApp/CCAboutPage.m
+++ 42CCApp/CCAboutPage.m
@@ -28,7 +28,29 @@
     [super viewDidLoad];
     UITextView *aboutView = [[UITextView alloc] initWithFrame:self.view.bounds];
     aboutView.tag = 50;
+    aboutView.delegate = self;
+    aboutView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    aboutView.text = [[NSUserDefaults standardUserDefaults] objectForKey:@"about"];
     [self.view addSubview:aboutView];
+}
+
+-(BOOL)         textView:(UITextView *)textView
+ shouldChangeTextInRange:(NSRange)range
+         replacementText:(NSString *)text{
+    if ([text isEqualToString:@"\n"]){
+        [textView resignFirstResponder];
+//        [self saveAboutData:textView.text];
+    }
+    return YES;
+}
+-(void) saveAboutData:(NSString *) text{
+    [[NSUserDefaults standardUserDefaults] setObject:text
+                                              forKey:@"about"];
+}
+
+-(BOOL) textViewShouldEndEditing:(UITextView *)textView{
+    [self saveAboutData:textView.text];
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
